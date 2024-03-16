30b772d1ed21eb0eb8ff996ff8f8b8d16dfc4cd7
Breye Foka <blagloire1@gmail.com>
2024-03-16 08:46:08 +0000

    setting up the env and creating the Sign-Up page

---
+++ login-form/src/SignUp.jsx
@@ -0,0 +1,41 @@
+
+export default function SignUp() {
+  return (
+    <div className='d-flex justify-content-center  align-items-center bg-secondary vh-100'>
+        <div className='bg-white p-3 rounded w-25 '>
+            <h2>
+            Sign Up
+            </h2>
+            <form>
+                <div className='mb-3'>
+                    <label htmlFor="email">
+                        <strong>Name</strong>
+                    </label>
+                    <input type="text" 
+                            placeholder='Enter Your Name' 
+                            name='email' className='form-control rounded-0' />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Email</strong>
+                    </label>
+                    <input type="text" 
+                            placeholder='Enter Your Email' 
+                            name='email' className='form-control rounded-0' />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password">
+                        <strong>Password</strong>
+                    </label>
+                    <input type="password" 
+                            placeholder='Enter Your Password' 
+                            name='password' className='form-control rounded-0' />
+                </div>
+                <button type='submit' className='btn btn-success w-100 rounded-0'> Register</button>
+                <p>Already Signed Up? </p>
+                <button className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Login</button>
+            </form>
+        </div>
+    </div>
+  )
+}
